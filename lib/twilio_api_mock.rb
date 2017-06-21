f63b7c58aa11965de7d5b8c4da3494e8ca5042c1
angel brown <angelkbrown@gmail.com>
2017-06-21 18:08:43 -0400

    Require valid params
    Make list method require string for 'date_sent'

--- lib/twilio_api_mock.rb
+++ lib/twilio_api_mock.rb
@@ -6,6 +6,7 @@ module TwilioApiMock
     attr_reader :redis
 
     MESSAGE_INDEX_SET = 'message_index'
+    DAY_IN_SECONDS = 24*60*60
 
     def initialize(redis_instance)
       @redis = redis_instance
@@ -16,6 +17,10 @@ module TwilioApiMock
     # "date_sent" (time sent) which stores the id for the hashed message.
     # https://redis.io/topics/indexes#composite-indexes
     def create(from:, to:, body:, date_sent: Time.now)
+      raise 'from must be a String' unless from.is_a?(String)
+      raise 'to must be a String' unless to.is_a?(String)
+      raise 'date_sent must be a Time' unless date_sent.is_a?(Time)
+
       message_id = "message:#{next_message_id}"
 
       # Add composite index
@@ -34,6 +39,12 @@ module TwilioApiMock
     # This returns all messages for a given 'to' which
     # occur at any time on a given date.
     def list(to:, date_sent:)
+      raise 'to must be a String' unless to.is_a?(String)
+      #TODO: Check that date_sent matches expected format.
+      # (If date_sent is a string without the expected format, the start date
+      # will be something like 0000-01-01.
+      raise 'date_sent must be a String with format yyyy-mm-dd' unless date_sent.is_a?(String)
+
       # Get beginning and end of date_sent
       range_start, range_end = unix_time_range(date_sent)
 
@@ -59,9 +70,9 @@ module TwilioApiMock
 
     # Returns [beginning of day, end of day]
     def unix_time_range(date)
-      range_start = date.to_time.to_i
-      range_end = (date + 1).to_time.to_i
-      [range_start, range_end]
+      range_start = Time.parse(date)
+      range_end = range_start + DAY_IN_SECONDS
+      [range_start.to_i, range_end.to_i]
     end
 
     # Increment message id
--- spec/twilio_api_mock_spec.rb
+++ spec/twilio_api_mock_spec.rb
@@ -1,8 +1,6 @@
 require "spec_helper"
 
 RSpec.describe TwilioApiMock::Messages do
-  DAY_IN_SECONDS = 24*60*60
-
   let(:fake_redis) { Redis.new }
   let(:messages_mock) { TwilioApiMock::Messages.new(fake_redis) }
 
@@ -25,18 +23,44 @@ RSpec.describe TwilioApiMock::Messages do
       messages_mock.create(from: from, to: to, body: body, date_sent: date_sent)
     end
 
-    it 'creates a hash in redis with id message:1' do
-      create
-      expect(fake_redis.hgetall('message:1')).to eq(expected_redis_record)
+    context 'valid params' do
+      it 'creates a hash in redis with id message:1' do
+        create
+        expect(fake_redis.hgetall('message:1')).to eq(expected_redis_record)
+      end
+
+      it 'creates the composite index sorted set element' do
+        create
+        index = fake_redis.zrangebylex(TwilioApiMock::Messages::MESSAGE_INDEX_SET,
+                                       "(#{to}:#{date_sent.to_i}",
+                                       "(#{to}:#{(date_sent + 1).to_i}")
+
+        expect(index.any?).to eq(true)
+      end
     end
 
-    it 'creates the composite index sorted set element' do
-      create
-      index = fake_redis.zrangebylex(TwilioApiMock::Messages::MESSAGE_INDEX_SET,
-                                     "(#{to}:#{date_sent.to_i}",
-                                     "(#{to}:#{(date_sent + 1).to_i}")
+    context 'invalid params' do
+      context '"to" is invalid' do
+        let(:to) { 1 }
 
-      expect(index.any?).to eq(true)
+        it 'raises an error' do
+          expect { create }.to raise_error 'to must be a String'
+        end
+      end
+
+      context '"from" is invalid' do
+        let(:from) { 1 }
+        it 'raises an error' do
+          expect { create }.to raise_error 'from must be a String'
+        end
+      end
+
+      context '"date_sent" is invalid' do
+        let(:date_sent) { 'not a time' }
+        it 'raises an error' do
+          expect { create }.to raise_error 'date_sent must be a Time'
+        end
+      end
     end
 
     context 'multiple messages are created' do
@@ -59,7 +83,7 @@ RSpec.describe TwilioApiMock::Messages do
 
   describe '#list' do
     let(:to_param) { message_to }
-    let(:date_sent_param) { Date.new(2017, 6, 1) }
+    let(:date_sent_param) { Date.new(2017, 6, 1).to_s }
     let(:message_to) { '+15559990000' }
     let(:message_time_sent) { Time.new(2017, 6, 1, 15, 0, 10, '+00:00') }
     let(:message_body) { 'Message' }
@@ -79,109 +103,129 @@ RSpec.describe TwilioApiMock::Messages do
       messages_mock.list(to: to_param, date_sent: date_sent_param)
     end
 
-    context 'messages do not exist' do
-      it { is_expected.to be_empty }
+    context 'invalid params' do
+      context '"to" is invalid' do
+        let(:to_param) { 1 }
+
+        it 'raises an error' do
+          expect { list }.to raise_error 'to must be a String'
+        end
+      end
+
+      context '"date_sent" is invalid' do
+        let(:date_sent_param) { Time.now.to_date }
+
+        it 'raises an error' do
+          expect { list }.to raise_error 'date_sent must be a String with format yyyy-mm-dd'
+        end
+      end
     end
 
-    context 'messages exist' do
-      before do
-        # Add message and its composite index
-        message_id = '123'
-        fake_redis.zadd(TwilioApiMock::Messages::MESSAGE_INDEX_SET, 0,
-                        "#{message_to}:#{unix_timestamp}:#{message_id}")
-
-        fake_redis.hmset("message:#{message_id}",
-                         'to', message_to,
-                           'date_sent', message_time_sent,
-                           'body', message_body 
-                        )
-      end
-
-      context 'message exists for requested date and "to"' do
-        let(:to_param) { message_to }
-        let(:date_sent_param) { message_time_sent.to_date }
-
-        it 'returns an array containing the message as a ruby object' do
-          expect(list).to eq(expected_list)
-        end
-      end
-
-      context 'message exists with requested "to" on preceeding date' do
-        let(:message_time_sent) do
-          (Time.new(2017, 6, 1, 15, 0, 10, "+00:00") - DAY_IN_SECONDS).utc
-        end
-
+    context 'valid params' do
+      context 'messages do not exist' do
         it { is_expected.to be_empty }
       end
 
-      context 'message exists with requested "to" on date after' do
-        let(:message_time_sent) do
-          (Time.new(2017, 6, 1, 15, 0, 10, "+00:00") + DAY_IN_SECONDS).utc
-        end
-
-        it { is_expected.to be_empty }
-      end
-
-      context 'no message exists for requested "to"' do
-        let(:to_param) { '+19999999999' }
-
-        it { is_expected.to be_empty }
-      end
-
-      context 'multiple messages on the same day' do 
-        let(:message2_time_sent) { Time.new(2017, 6, 1, 16, 0, 10, '+00:00') }
-        let(:message2_body) { 'Message 2' }
-        let(:message2_object) do
-          OpenStruct.new(
-            {
-              'to' => message_to,
-              'date_sent' => "#{message2_time_sent}",
-              'body' => message2_body
-            }
-          )
-        end 
-        let(:expected_list) { [message_object, message2_object] }
-
+      context 'messages exist' do
         before do
-          message2_id = '345'
+          # Add message and its composite index
+          message_id = '123'
           fake_redis.zadd(TwilioApiMock::Messages::MESSAGE_INDEX_SET, 0,
-                          "#{message_to}:#{message2_time_sent.to_i}:#{message2_id}")
+                          "#{message_to}:#{unix_timestamp}:#{message_id}")
 
-          fake_redis.hmset("message:#{message2_id}",
+          fake_redis.hmset("message:#{message_id}",
                            'to', message_to,
-                             'date_sent', message2_time_sent,
-                             'body', message2_body
+                             'date_sent', message_time_sent,
+                             'body', message_body
                           )
         end
 
-        it 'returns all messages with requested "to"' do
-          expect(list.include?(message_object)).to eq(true)
-          expect(list.include?(message2_object)).to eq(true)
+        context 'message exists for requested date and "to"' do
+          let(:to_param) { message_to }
+          let(:date_sent_param) { message_time_sent.to_date.to_s }
+
+          it 'returns an array containing the message as a ruby object' do
+            expect(list).to eq(expected_list)
+          end
         end
 
-        context 'additional messages on same day with different "to"' do
-          let(:different_message_to) { '+18889997777' }
+        context 'message exists with requested "to" on preceeding date' do
+          let(:message_time_sent) do
+            (Time.new(2017, 6, 1, 15, 0, 10, "+00:00") - TwilioApiMock::Messages::DAY_IN_SECONDS).utc
+          end
+
+          it { is_expected.to be_empty }
+        end
+
+        context 'message exists with requested "to" on date after' do
+          let(:message_time_sent) do
+            (Time.new(2017, 6, 1, 15, 0, 10, "+00:00") + TwilioApiMock::Messages::DAY_IN_SECONDS).utc
+          end
+
+          it { is_expected.to be_empty }
+        end
+
+        context 'no message exists for requested "to"' do
+          let(:to_param) { '+19999999999' }
+
+          it { is_expected.to be_empty }
+        end
+
+        context 'multiple messages on the same day' do
+          let(:message2_time_sent) { Time.new(2017, 6, 1, 16, 0, 10, '+00:00') }
+          let(:message2_body) { 'Message 2' }
+          let(:message2_object) do
+            OpenStruct.new(
+              {
+                'to' => message_to,
+                'date_sent' => "#{message2_time_sent}",
+                'body' => message2_body
+              }
+            )
+          end
+          let(:expected_list) { [message_object, message2_object] }
 
           before do
-            different_message_id = 999
+            message2_id = '345'
             fake_redis.zadd(TwilioApiMock::Messages::MESSAGE_INDEX_SET, 0,
-                            "#{different_message_to}:#{message2_time_sent.to_i}:#{different_message_id}")
+                            "#{message_to}:#{message2_time_sent.to_i}:#{message2_id}")
 
-            fake_redis.hmset("#{different_message_id}",
-                             'to', different_message_to,
-                             'date_sent', message2_time_sent,
-                             'body', 'Different message'
+            fake_redis.hmset("message:#{message2_id}",
+                             'to', message_to,
+                               'date_sent', message2_time_sent,
+                               'body', message2_body
                             )
           end
 
-          it 'does not return message with different "to"' do
-            expect(list.select{ |msg| msg.to == message_to }.any?).to eq(true)
-            expect(list.select{ |msg| msg.to == different_message_to }.any?).to eq(false)
-          end
-
-          it 'still returns all messages with requested "to"' do
+          it 'returns all messages with requested "to"' do
             expect(list.include?(message_object)).to eq(true)
             expect(list.include?(message2_object)).to eq(true)
+          end
+
+          context 'additional messages on same day with different "to"' do
+            let(:different_message_to) { '+18889997777' }
+
+            before do
+              different_message_id = 999
+              fake_redis.zadd(TwilioApiMock::Messages::MESSAGE_INDEX_SET, 0,
+                              "#{different_message_to}:#{message2_time_sent.to_i}:#{different_message_id}")
+
+              fake_redis.hmset("#{different_message_id}",
+                               'to', different_message_to,
+                               'date_sent', message2_time_sent,
+                               'body', 'Different message'
+                              )
+            end
+
+            it 'does not return message with different "to"' do
+              expect(list.select{ |msg| msg.to == message_to }.any?).to eq(true)
+              expect(list.select{ |msg| msg.to == different_message_to }.any?).to eq(false)
+            end
+
+            it 'still returns all messages with requested "to"' do
+              expect(list.include?(message_object)).to eq(true)
+              expect(list.include?(message2_object)).to eq(true)
+            end
           end
         end
       end
